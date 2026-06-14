263f43793cd735a87c62f84e6a92237570e2d151
sixgagbtw <95422287+sixgagbtw@users.noreply.github.com>
2026-06-14 20:03:01 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+pages = [
+    st.Page("analysis_and_model.py", title="Анализ и модель"),
+    st.Page("presentation.py", title="Презентация"),
+]
+
+current_page = st.navigation(pages, position="sidebar", expanded=True)
+current_page.run()
